38cb2b8dca5909f611801232d89452178cfe869d
santinabardengo <santibardengo@gmail.com>
2024-10-15 20:09:33 -0300

    commit 6

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
