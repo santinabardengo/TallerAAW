f1ecdd056e99fb531de8b7a574141cd9dac352c8
ines1024 <inegalarza@gmail.com>
2024-10-20 17:16:06 -0300

    formulario css y ts

--- frontend/src/app/formulario/formulario.component.ts
+++ frontend/src/app/formulario/formulario.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf
 import { FormsModule } from '@angular/forms'; 
+import { PoiService } from '../services/poi.service'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -16,9 +18,40 @@ export class FormularioComponent {
   mostrarFechaEvento = false;
   horarioApertura = '';
   horarioCierre = '';
+  nombre = '';
+  direccion = '';
+  descripcion = '';
+  fechaEvento? = '';
+
 
   CambioCategoria(){
     this.mostrarFechaEvento = this.categoriaSeleccionada === 'evento';
+  }
+
+  constructor(private poiService: PoiService, private router: Router){}
+  
+  //enviar el formulario cuando se copmlete
+
+  enviarFormulario(){
+    const newPoi = {
+      nombre: this.nombre,
+      direccion: this.direccion,
+      categoria: this.categoriaSeleccionada,
+      descripcion: this.descripcion,
+      horarioApertura: this.horarioApertura,
+      horarioCierre: this.horarioCierre,
+      fechaEvento: this.fechaEvento
+    };
+    
+    this.poiService.createPOI(newPoi).subscribe({
+      next: (response) => {
+        console.log("POI creado", response);
+        this.router.navigate(['/map']);
+      },
+      error: (err) => {
+      console.error("error al crear poi", err);
+    }
+    });
   }
 
 }
