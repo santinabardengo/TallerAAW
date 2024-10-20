e5c5986e9d8728921f8a867bb27272b7f1365c7f
ines1024 <inegalarza@gmail.com>
2024-10-20 13:01:20 -0300

    formulario mejoras

--- frontend/src/app/formulario/formulario.component.ts
+++ frontend/src/app/formulario/formulario.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, FormsModule
+  ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  categoriaSeleccionada = '';
+  mostrarFechaEvento = false;
+  horarioApertura = '';
+  horarioCierre = '';
+
+  CambioCategoria(){
+    this.mostrarFechaEvento = this.categoriaSeleccionada === 'evento';
+  }
 
 }
