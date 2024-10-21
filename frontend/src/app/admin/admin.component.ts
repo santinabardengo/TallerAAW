fc3db3c629a3626420691552555bee3383f259ec
santinabardengo <santibardengo@gmail.com>
2024-10-20 22:11:37 -0300

    Aprobado POI

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { PoiService } from '../services/poi.service'; 
-import { CommonModule } from '@angular/common'; 
+import { PoiService } from '../services/poi.service';
+import { CommonModule } from '@angular/common';
 import { MapComponent } from '../map/map.component';
 
 @Component({
@@ -10,20 +10,20 @@ import { MapComponent } from '../map/map.component';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-
 export class AdminComponent {
   constructor(private poiService: PoiService) {}
 
-  pendingPOIs: any[] = []; 
-  showPendingPOIs: boolean = false; 
+  pendingPOIs: any[] = [];
+  showPendingPOIs: boolean = false;
+  confirmationMessage: string | null = null;
 
   loadPendingPOIs() {
     console.log('Cargando POIs pendientes...');
     this.poiService.getPendingPOIs().subscribe({
       next: (pois) => {
-        console.log('Datos recibidos:', pois); 
-        this.pendingPOIs = pois; 
-        this.showPendingPOIs = true; 
+        console.log('Datos recibidos:', pois);
+        this.pendingPOIs = pois;
+        this.showPendingPOIs = true;
       },
       error: (err) => {
         console.error('Error al cargar POIs pendientes:', err);
@@ -31,11 +31,12 @@ export class AdminComponent {
     });
   }
 
-  //aprobar poi
   approvePOI(nombre: string) {
     this.poiService.approvePOI(nombre).subscribe({
       next: (response) => {
         console.log('POI aprobado:', response);
+        this.showConfirmation(`POI "${nombre}" aprobado`);
+        this.removePOI(nombre);
       },
       error: (err) => {
         console.error('Error al aprobar POI:', err);
@@ -43,15 +44,26 @@ export class AdminComponent {
     });
   }
 
-  // Método para rechazar un POI
   rejectPOI(nombre: string) {
     this.poiService.rejectPOI(nombre).subscribe({
       next: (response) => {
         console.log('POI rechazado:', response);
+        this.showConfirmation(`POI "${nombre}" rechazado`);
+        this.removePOI(nombre);
       },
       error: (err) => {
         console.error('Error al rechazar POI:', err);
       }
     });
   }
+
+  showConfirmation(message: string) {
+    this.confirmationMessage = message;
+    setTimeout(() => (this.confirmationMessage = null), 5000); // Desaparece en 3 segundos
+  }
+
+  removePOI(nombre: string) {
+    this.pendingPOIs = this.pendingPOIs.filter((poi) => poi !== nombre);
+  }
 }
+
