28ecdeaa7799b47f3f83a7cd524c3fb0fc8baa5f
santinabardengo <santibardengo@gmail.com>
2024-10-22 11:46:45 -0300

    vista POI

--- frontend/src/app/user-map/user-map.component.ts
+++ frontend/src/app/user-map/user-map.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PoiService } from '../services/poi.service';
+import { CommonModule } from '@angular/common';
+import { MapComponent } from '../map/map.component';
+
+interface PointOfInterest {
+  name: string;
+  description: string;
+  direccion: string;
+  horarioApertura: string;
+  horarioCierre: string;
+}
 
 @Component({
-  selector: 'app-user-map',
+  selector: 'user-map',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MapComponent],
   templateUrl: './user-map.component.html',
-  styleUrl: './user-map.component.css'
+  styleUrls: ['./user-map.component.css']
 })
-export class UserMapComponent {
 
+export class UserMapComponent implements OnInit {
+  puntosDeInteres: PointOfInterest[] = [];
+
+  constructor(private poiService: PoiService) {}
+
+  ngOnInit(): void {
+    // Obtener los puntos de interés del servicio
+    this.poiService.getApprovedPOIs().subscribe(
+      (puntos: PointOfInterest[]) => {
+        this.puntosDeInteres = puntos; 
+      },
+      (error) => {
+        console.error('Error al obtener puntos de interés:', error);
+      }
+    );
+  }
 }
